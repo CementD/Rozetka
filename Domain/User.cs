877dcdfd96de162f18a8d1d9b6263ad7633cd664
Deny2503 <bilotserkovskiydimayt@gmail.com>
2026-06-11 12:17:49 +0300

    update

--- Domain/User.cs
+++ Domain/User.cs
@@ -4,10 +4,10 @@ namespace Domain
 {
     public class User : IdentityUser
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Address { get; set; }
-        public string PhoneNumber { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Address { get; set; }
+        public new string? PhoneNumber { get; set; }
         public ICollection<Order> Orders { get; set; } = new List<Order>();
     }
 }
